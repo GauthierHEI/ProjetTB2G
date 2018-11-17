f777ee82058b65e6b8c490a031c0db3c1a0f9331
Bapth_dej <baptiste.desjobert@hei.yncrea.fr>
2018-11-17 15:08:17 +0100

    populate table correct

--- src/test/java/TB2G/UtilisateurDaoTestCase.java
+++ src/test/java/TB2G/UtilisateurDaoTestCase.java
@@ -50,7 +50,7 @@ public class UtilisateurDaoTestCase {
         //THEN
         assertThat(utilisateurs).hasSize(2);
         assertThat(utilisateurs).extracting("email","prenom","nom","naissance","motdepasse","adresseliv","adressefac","admin").containsOnly(
-                tuple("gauthier.hacout@hei.yncrea.fr","Alex","Alex", LocalDate.of(1997, 10, 21),"$argon2i$v=19$m=65536,t=5,p=1$QvOv3+9qSYxIzLlzjeJKc8qpT9PJ42y9B+U/hXBejlB3mSghQ75TcJemyV2X51pN2sWspKHwqgWXtddCykMUsWBajRljlm9EJEPRZhhOi4TjkUsqcbto8ToE3ih7FiBIt4sQF1pMW4BwoXZOtQ1/jc6p53h2zmOJ6szzuzhQlC0$lqf32VJjfnSEeu2Nnnck0nTrpWJuhmpSa/XPx86cBjpgj2UCR4fSCoeeffXj4i4eEV/RKIidgzbW4tUJ07VfDA1khGZ+N+/6IC21QM+jcPdhCQd2wFOS6T3vHGj+2Gw5yFREJeh9wSZTf+lAZtFKGS3Ca1E6EJSlQBgtAzJlzz0","10 rue Adolphe","10 rue Adolphe",false),
+                tuple("alex@alex.fr","Alex","Alex", LocalDate.of(1997, 10, 21),"$argon2i$v=19$m=65536,t=5,p=1$QvOv3+9qSYxIzLlzjeJKc8qpT9PJ42y9B+U/hXBejlB3mSghQ75TcJemyV2X51pN2sWspKHwqgWXtddCykMUsWBajRljlm9EJEPRZhhOi4TjkUsqcbto8ToE3ih7FiBIt4sQF1pMW4BwoXZOtQ1/jc6p53h2zmOJ6szzuzhQlC0$lqf32VJjfnSEeu2Nnnck0nTrpWJuhmpSa/XPx86cBjpgj2UCR4fSCoeeffXj4i4eEV/RKIidgzbW4tUJ07VfDA1khGZ+N+/6IC21QM+jcPdhCQd2wFOS6T3vHGj+2Gw5yFREJeh9wSZTf+lAZtFKGS3Ca1E6EJSlQBgtAzJlzz0","10 rue Adolphe","10 rue Adolphe",false),
                 tuple("admin@hei.yncrea.fr","Admin","Admin",LocalDate.of(1978, 05, 24),"$argon2i$v=19$m=65536,t=5,p=1$h6yY9UMpKwAFiKSM9vrBpDnOaxAAcvMzkDIxK2hjg+b0NrRI7Hutfpz8PS+rchimUndfBfHdN/JQDQVpuBEl7DL3dxhOmG6yVEmO0GZ1PTJq3fT/kNi2bcbikEjp+ttCHgMKMYvab5iW2VsDjwUbinxb0PixDqDOnZYCV5U3SIE$q8Rv5q8v71NIFpzVnHjGI1MaJQOq8b9fJZyzuzHi9GN/NEr1IMFuYjlpneLvnXpKECtvqNq+s/cYuUiWqFJvsgl1j3HumqnubfXeTi5q20N3LCZis1EaZKd1pYsmpF8vysVdQe9V5GofVrrjCwIohoq+3Qj49rQb+GBCBVyJmpw","13 rue de Toul","13 rue de Toul",true)
         );
     }
